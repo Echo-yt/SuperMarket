3db166c17b9eb09ccdb843cea9d4c5877162087a
Echo-yt <Echoyt@163.com>
2019-11-27 17:51:54 +0800

    11.27

--- src/main/java/com/zhg/javakc/modules/supermarket/commodity/entity/CommodityEntity.java
+++ src/main/java/com/zhg/javakc/modules/supermarket/commodity/entity/CommodityEntity.java
@@ -5,53 +5,52 @@ import com.zhg.javakc.modules.supermarket.commodity_type.entity.TypeEntity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.ManyToOne;
 import java.util.List;
 
 @Getter
 @Setter
 public class CommodityEntity extends BaseEntity<CommodityEntity> {
     //商品id
-  String commodityId;
-  //商品名字
-  String commodityName;
-  //商品销售价格
-    String commodityPrice ;
+    String commodityId;
+    //商品名字
+    String commodityName;
+    //商品销售价格
+    String commodityPrice;
     //商品简介
-   String commodityDetail;
-   //关键字
-   String commodityKeyword ;
-   //参考价
-   double commodityRprice;
-   //成本价
-   double commodityCprice ;
-   //是否支持退货
+    String commodityDetail;
+    //关键字
+    String commodityKeyword;
+    //参考价
+    double commodityRprice;
+    //成本价
+    double commodityCprice;
+    //是否支持退货
     Integer commoditySupport;
     //供应商id
-   String supplierId ;
-   //品牌
-   String commodityBrand;
-   //产地
+    String supplierId;
+    //品牌
+    String commodityBrand;
+    //产地
     String commodityPlace;
     //水体
-   String commodityWater;
+    String commodityWater;
     //净含量
-  Integer  commodityContent ;
-  //存储条件
-  String  commodityStorage;
-  //保质期
-   Integer commodityQuality;
-   //包装
+    Integer commodityContent;
+    //存储条件
+    String commodityStorage;
+    //保质期
+    Integer commodityQuality;
+    //包装
     String commodityPackage;
     //单位
     String commodityCompany;
     //文章id
     String articleId;
-   //分类
+    //分类
     List<TypeEntity> typeList;
     String typeFirst;
     String typeSecond;
-   //状态 0:未上架 1：已上架 2：已下架 3：删除 进入回收站
+    //状态 0:未上架 1：已上架 2：已下架 3：删除 进入回收站
     Integer commodityState;
     //安全库存
     Integer commoditySafeStock;
